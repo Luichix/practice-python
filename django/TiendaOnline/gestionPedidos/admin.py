6f68bb6f7b198ebdc970e5426d08ff5a541ebe7a
Luichix <luichix.rex@gmail.com>
2022-07-27 16:18:06 -0600

    class 17-25

--- django/TiendaOnline/gestionPedidos/admin.py
+++ django/TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from gestionPedidos.models import Clientes, Articulos, Pedidos
+
 # Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'telefono')
+    search_fields = ('nombre', 'telefono')
+    #ordering = ('nombre',)
+    #fieldsets = (
+    #    ('Cliente', {'fields': ('nombre', 'direccion', 'email', 'telefono')}),
+    #)
+    #class Meta:
+    #    model = Clientes
+
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter = ('seccion',)
+
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'fecha')
+    list_filter = ('fecha',)
+    date_hierarchy = 'fecha'
+    # ordering = ('numero',)
+    #fieldsets = (
+    #    ('Pedido', {'fields': ('numero', 'fecha', 'entregado')}),
+    #)
+    #class Meta:
+    #    model = Pedidos
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
