ce5ea6607987357e5ad8cf4e659b26d78f1cf589
Luichix <luisreynaldo.pch@gmail.com>
2023-09-10 22:07:46 -0600

    query params and body fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,14 +1,23 @@
 from enum import Enum
-
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 app = FastAPI()
 
+fake_elements_db = [
+    {"element_name": "Foo"},
+    {"element_name": "Bar"},
+    {"element_name": "Boo"},
+]
+
+
 class Item(BaseModel):
     name: str
+    description: str | None = None
     price: float
-    is_offer: bool | None = None 
+    is_offer: bool | None = None
+    tax: float | None = None
 
 
 class ModelName(str, Enum):
@@ -16,13 +25,16 @@ class ModelName(str, Enum):
     resnet = "resnet"
     letnet = "letnet"
 
-@app.get("/") 
+
+@app.get("/")
 async def read_root():
-    return { "Hello" : "World"}
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str |None = None):
-    return {"item_id": item_id, "q": q }
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
 
 @app.put("/items/{items_id}")
 async def update_item(item_id: int, item: Item):
@@ -33,24 +45,109 @@ async def update_item(item_id: int, item: Item):
 async def read_users():
     return ["Luichix", "Manu"]
 
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "The current user"}
 
+
 @app.get("/users/{user_id}")
-async def read_user(user_id:str):
+async def read_user(user_id: str):
     return {"user_id": user_id}
 
+
 @app.get("/models/{model_name}")
-async def get_model(model_name:ModelName):
+async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FWT!" }
-    
+        return {"model_name": model_name, "message": "Deep Learning FWT!"}
+
     if model_name is ModelName.letnet:
-        return {"model_name": model_name, "message": "LeCNN all the images" }
-    
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
     return {"model_name": model_name, "message": "Have some residuals"}
+
 
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/elements/")
+async def read_element(skip: int = 0, limit: int = 10):
+    return fake_elements_db[skip : skip + limit]
+
+
+@app.get("/items/query/{item_id}")
+async def read_itemquery(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{items_id}")
+async def read_user_items(
+    user_id: str, item_id: str, needy: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "user_id": user_id, "needy": needy}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is a long description for this item"})
+    return item
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax: item.tax + item.price
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/body/{item_id}")
+async def update_item_body(item_id: str, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
+
+
+@app.put("/items/body_query/{item_id}")
+async def update_item_body_query(item_id: str, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/fruit/")
+async def read_fruit(
+    q: Annotated[str | None, Query(min_length=3, max_length=50, pattern="\S")] = None
+):
+    # For Required params use: "Required" or Elipsis "..."
+    # Also you can remove Nove or Query(default=None)
+    # Can you use both: Declare type None and set Requerid Param
+    result = {"fruits": [{"fruit_name": "apple"}, {"fruit_name": "banana"}]}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/fruits/list/")
+async def read_fruits_list(
+    q: Annotated[
+        list[str],
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            alias="item-query",
+            deprecated=True,
+            include_in_schema=True,
+        ),
+    ] = ["apple", "banana"]
+):
+    query_items = {"q": q}
+    return query_items
